34b97cd35015eba0306322988fed9c332cd500fa
G.Unknown@gmail.com <G.Unknown@gmail.com@1a89f644-db0b-5292-bd5e-bace5130ee07>
2012-04-23 18:33:52 +0000

    Scout English voice command captions completed. Initial binary compilation. Updated compile.bat to check for steam.bat, fails with error code 1 if not present and exits with code 0 normally. Now writes the entire log to disk, and prunes old log on startup.

--- compile.bat
+++ compile.bat
@@ -1,12 +1,15 @@
 @echo off
-:: Close caption compiler - place in tf/resource
+:: Closed caption compiler - place in tf/resource
 :: i-ghost
-call steam
+if not exist steam.bat (echo Fatal: steam.bat required in PATH or current directory. & exit /b 1) else (call steam)
 set VPROJECT=%_steamdir%\steamapps\%_steamuser%\team fortress 2\tf
 set bin=%sourcesdk%\bin\orangebox\bin\captioncompiler.exe
-for /f %%a in ('dir /b closecaption_*.txt') do (echo compiling %%a... & %bin% -l "%%~fa" >nul)
+del /F /S /Q log.txt
+for /f %%a in ('dir /b closecaption_*.txt') do (echo Compiling Captions for %%a... & %bin% "%%~fa" >>log.txt)
+:: Create backups in the (highly unlikely) event that Valve update the caption files.
 echo Creating backups...
 if not exist .\backup md .\backup
 xcopy closecaption_*.txt .\backup\ /Y /Q >nul
 xcopy closecaption_*.dat .\backup\ /Y /Q >nul
 echo Done.
+exit /b 0
