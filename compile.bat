add04cd2a1a7ab085bedf66804aedba0e63c199d
G.Unknown@gmail.com <G.Unknown@gmail.com@1a89f644-db0b-5292-bd5e-bace5130ee07>
2012-04-23 18:43:54 +0000

    Updated compile.bat to be less verbose about log pruning.

--- compile.bat
+++ compile.bat
@@ -4,7 +4,7 @@
 if not exist steam.bat (echo Fatal: steam.bat required in PATH or current directory. & exit /b 1) else (call steam)
 set VPROJECT=%_steamdir%\steamapps\%_steamuser%\team fortress 2\tf
 set bin=%sourcesdk%\bin\orangebox\bin\captioncompiler.exe
-del /F /S /Q log.txt
+del /F /S /Q log.txt >nul
 for /f %%a in ('dir /b closecaption_*.txt') do (echo Compiling Captions for %%a... & %bin% "%%~fa" >>log.txt)
 :: Create backups in the (highly unlikely) event that Valve update the caption files.
 echo Creating backups...
